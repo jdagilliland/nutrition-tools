1a4d9831613716394a842f8dfb047eab5eb0bc6f
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2016-12-07 00:36:49 -0500

    Flesh out more convenience functions
    I've included a monoid instance, as well as begun flirting with finally
    parsing data from a CSV.

--- Meals.hs
+++ Meals.hs
@@ -1,6 +1,8 @@
 module Meals where
 
 import Control.Applicative
+import Data.Monoid
+import Data.Csv
 
 data Macros = Macros { fat :: Float
                      , carbs :: Float
@@ -12,24 +14,37 @@ data MacroData = MacroData { macros :: Macros
                            , mass :: Float
                            } deriving Show
 
-scaleByMass :: Float -> MacroData -> MacroData
-scaleByMass newMass (MacroData macro portionVolume portionMass) = let
-    scale = newMass / portionMass
-    newFat = (fat macro) * scale
-    newCarbs = (carbs macro) * scale
-    newProtein = (protein macro) * scale
-    newVolume = fmap (* scale) portionVolume
-    in
-    MacroData { macros = Macros { fat = newFat
-                                , carbs = newCarbs
-                                , protein = newProtein
-                                }
-              , volume = newVolume
-              , mass = newMass
+instance Monoid MacroData where
+    mempty = MacroData (Macros 0 0 0) (Just 0) 0
+    mappend = combineFoods
+
+scaleMacros :: Float -> Macros -> Macros
+scaleMacros scale (Macros oldFat oldCarbs oldProtein) =
+    Macros (oldFat * scale) (oldCarbs * scale) (oldProtein * scale)
+
+scaleMacroData :: Float -> MacroData -> MacroData
+scaleMacroData scale (MacroData oldMacros oldVol oldMass) =
+    MacroData { macros = scaleMacros scale oldMacros
+              , volume = fmap (* scale) oldVol
+              , mass = oldMass * scale
               }
 
+scaleByMass :: Float -> MacroData -> MacroData
+scaleByMass newMass macroData = let
+    scale = newMass / mass macroData
+    in
+    scaleMacroData scale macroData
+
+scaleByVol :: Float -> MacroData -> MacroData
+scaleByVol vol macroData = undefined
+-- scaleByVol vol macroData = let
+--     scale = fmap (\x -> vol / x) $ volume macroData
+--     in
+--     scaleMacroData scale macroData
+
 combineFoods :: MacroData -> MacroData -> MacroData
-combineFoods (MacroData macro1 vol1 mass1) (MacroData macro2 vol2 mass2) = let
+combineFoods (MacroData macro1 vol1 mass1) (MacroData macro2 vol2 mass2) =
+    let
     newFat = fat macro1 + fat macro2
     newCarbs = carbs macro1 + carbs macro2
     newProtein = protein macro1 + protein macro2
@@ -37,14 +52,20 @@ combineFoods (MacroData macro1 vol1 mass1) (MacroData macro2 vol2 mass2) = let
                        , carbs = newCarbs
                        , protein = newProtein
                        }
-    newVolume = (+) <$> vol1 <*> vol2 -- revised to account for maybes
-    newMass = mass1 + mass2
     in
     MacroData { macros = combMacro
-              , volume = newVolume
-              , mass = newMass
+              , volume = (+) <$> vol1 <*> vol2
+              , mass = mass1 + mass2
               }
+
+calories :: Macros -> Float
+calories (Macros gFat gCarbs gProtein) =
+    9 * gFat + 4 * gCarbs + 4 * gProtein
 
 -- Delete below following early testing phase.
 oats = MacroData ( Macros 5 54 10 ) (Just 250) 80
 almonds = MacroData (Macros 15 6 6 ) Nothing 30
+blackBeans = MacroData ( Macros 1.8 66.5 24.5 ) Nothing 427
+riceCooked = MacroData ( Macros 0 45 4 ) (Just 250) 158
+bellPepper = MacroData ( Macros 0 10 2 ) Nothing 226
+-- mexicanRiceBeans = scaleByMass
